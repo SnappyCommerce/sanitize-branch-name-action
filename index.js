9632f74565998739f5158e01e9d08a796b0f2039
Neil Seligmann <git@neilseligmann.com>
2022-02-15 15:12:32 -0300

    feat: added truncate and '-' char removal

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const branchName = core.getInput('branch-name');
-  console.log(`branch-name: ${branchName}`);
-  const sanitizedBranchName = branchName.trim().toLowerCase().replace(/[\W_]+/g,"-").substring(0, 63);
-  core.setOutput("sanitized-branch-name", sanitizedBranchName);
+	const branchName = core.getInput('branch-name');
+	let maxLength = parseInt(core.getInput('max-length'), 10);
+	if (!maxLength || isNaN(maxLength)) maxLength = 30;
 
-  console.log(`${branchName} -> ${sanitizedBranchName}`);
+	console.log(`branch-name: ${branchName}`);
+	const sanitizedBranchName = branchName.trim().toLowerCase().replace(/[\W_]+/g, "-").substring(0, 63);
+	core.setOutput('sanitized-branch-name', sanitizedBranchName);
+
+	let truncated = sanitizedBranchName.substring(0, maxLength);
+	console.log('truncated', truncated);
+
+	// Remove ending '-'
+	while (truncated.slice(-1) === '-') {
+		truncated = truncated.slice(0, -1);
+	}
+
+	console.log(`${branchName} -> ${sanitizedBranchName}`);
 } catch (error) {
-  core.setFailed(error.message);
+	core.setFailed(error.message);
 }
